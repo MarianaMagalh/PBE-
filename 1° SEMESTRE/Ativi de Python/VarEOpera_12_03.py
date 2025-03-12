aa96cd1e6432ff8ee04d15984f6e3ff4d5b39163
56463939811 <jorgegameplay811@gmail.com>
2025-03-12 17:03:54 -0300

    organização do diretorio e add da atividade da semana

---
+++ 1° SEMESTRE/Ativi de Python/VarEOpera_12_03.py
@@ -0,0 +1,62 @@
+# exer 1
+nome = "João"
+
+print(nome)
+
+# exer 2
+a = 5
+b = 10
+
+soma = a + b
+sub = a - b
+multi = a * b
+divi = a / b
+
+print("\nOperações")
+print(f"Adição: {a} + {b} = {soma}\nSubtração: {a} - {b} = {sub}\nMultiplicação: {a} * {b} = {multi}\nDivisão: {a} / {b} = {divi}\n")
+
+# exer 3
+preco = 50
+desconto = 10
+
+res = preco * (10/100)
+
+print(res)
+
+# exer 4
+resultado = (10 + 5) * 2
+
+print(resultado)
+
+# exer 5
+texto = "150"
+convert = int(texto)
+
+conta = convert * 2
+
+print(conta)
+
+# exer 7
+'''
+a = int(input("\nDigite um valor a ser somado: "))
+b = int(input("Digite um valor segundo a ser somado: "))
+
+somar = a + b
+
+print(f"A soma dos valore é {somar}\n")
+'''
+
+# exer 8
+'''
+x = int(input("Insira um valor: "))
+y = int(input("Insira um outro valor: "))
+
+divi = x // y
+
+print(f"O resultado da divisão inteira é {divi}")
+'''
+# exer 9
+num1 = int(input("\nDigite um número: "))
+num2 = int(input("Digite um outro número: "))
+
+print(f"Caso o número {num1} for maior que {num2}, aparecerá true. Caso ao contrario aparecerá false.")
