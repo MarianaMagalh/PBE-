3d8697ed880dee20410531603ba88e8d5eca27d9
56463939811 <jorgegameplay811@gmail.com>
2025-03-19 15:06:30 -0300

    organização do diretorio e add da atividade da semana

--- 1° SEMESTRE/Ativi de Python/VarEOpera_12_03.py
+++ 1° SEMESTRE/Ativi de Python/VarEOpera_12_03.py
@@ -56,7 +56,91 @@ divi = x // y
 print(f"O resultado da divisão inteira é {divi}")
 '''
 # exer 9
+'''
 num1 = int(input("\nDigite um número: "))
 num2 = int(input("Digite um outro número: "))
 
-print(f"Caso o número {num1} for maior que {num2}, aparecerá true. Caso ao contrario aparecerá false.")
+print(f"Caso o número {num1} for maior que {num2}, irá aparecer TRUE.Caso ao contrario aparecerá FALSE.\n{num1 > num2}")
+
+'''
+
+# ex 10
+'''
+idade = int(input("Insira a sua idade: "))
+
+diasvivi = idade * 365
+
+print(f"Essa é a quantidade de dias que vc já viveu, {diasvivi}")
+'''
+
+# ex 11
+'''
+base = int(input("Insira a base: "))
+expo = int(input("Insira o expoente: "))
+
+resultado = base ** expo
+
+print(f"A potencia dos números é {resultado}")
+
+'''
+
+# ex 12
+'''
+preco = float(input("Insira o preço: "))
+
+stringPreco = str(preco)
+
+print(f"O preço é R${stringPreco}")
+'''
+
+
+# ex 13
+'''
+raio = float(input("Insira o raio: "))
+
+area = 3.1415 (raio ** 2)
+
+print(f"Area do circulo é {area}")
+'''
+
+# ex 14
+'''
+a = int(input("Digite um número: "))
+b = int(input("digite um outro número: "))
+
+a, b = b,a
+
+print(f"O resultado: {a} e {b}")
+'''
+
+# ex 15
+'''
+nota1 = float(input("Insira a primaira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+
+ps1 = 2
+ps2 = 3
+ps3 = 5
+
+medpo = (nota1 * ps1 + nota2 * ps2 + nota3 * ps3)/(ps1 + ps2 + ps3)
+
+print(f"A media ponderada é {medpo}")
+'''
+
+# desafio
+x1 = float(input("Insira o primeiro ponto: "))
+x2 = float(input("Insira o segundo ponto: "))
+y1 = float(input("Insira o terceiro ponto: "))
+y2 = float(input("Insira o quarto ponto: "))
+
+
+
+
+
+
+
+
+
+
+
